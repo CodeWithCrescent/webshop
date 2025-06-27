b4986f74436fc2513fcc619e29fd4649394a38aa
Crescent Geniius <crescentbeatz31@gmail.com>
2025-06-28 00:58:10 +0300

    Add BottomBar widget for enhanced navigation in the app

---
+++ lib/shared/widgets/bottom_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:awesome_bottom_bar/awesome_bottom_bar.dart';
+import 'package:webshop/core/constants/app_colors.dart';
+
+class BottomBar extends StatelessWidget {
+  final int selectedIndex;
+  final ValueChanged<int> onTap;
+
+  const BottomBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  static const List<TabItem> items = [
+    TabItem(icon: Icons.home, title: 'Home'),
+    TabItem(icon: Icons.inventory_2_outlined, title: 'Inventory'),
+    TabItem(icon: Icons.add, title: 'Cash Sales'),
+    TabItem(icon: Icons.people_alt_outlined, title: 'Customers'),
+    TabItem(icon: Icons.receipt_long, title: 'Z-Reports'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomBarCreative(
+      items: items,
+      backgroundColor: AppColors.surfaceLight,
+      color: AppColors.textSecondary,
+      colorSelected: AppColors.primary,
+      indexSelected: selectedIndex,
+      iconSize: 24,
+      isFloating: true,
+      highlightStyle:
+          const HighlightStyle(sizeLarge: true, background: AppColors.primary, elevation: 2),
+      onTap: onTap,
+    );
+  }
+}
