02f4ca50e8a64516a2bd50930f673532b23ae4be
Crescent Geniius <crescentbeatz31@gmail.com>
2025-06-28 03:14:45 +0300

    Remove unused Drawer widget and its associated items from LayoutPage

--- lib/shared/layout_page.dart
+++ lib/shared/layout_page.dart
@@ -30,55 +30,6 @@ class _LayoutPageState extends State<LayoutPage> {
         selectedIndex: _currentIndex,
         onTap: (index) => _onTabTapped(context, index, loc),
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: const EdgeInsets.all(0),
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.green,
-              ),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(color: Colors.green),
-                accountName: Text(
-                  "Zalongwa User",
-                  style: TextStyle(fontSize: 18),
-                ),
-                accountEmail: Text("ztl@gmail.com"),
-                currentAccountPictureSize: Size.square(50),
-                currentAccountPicture: CircleAvatar(
-                  backgroundColor: Color.fromARGB(255, 165, 255, 137),
-                  child: Text(
-                    "Z",
-                    style: TextStyle(fontSize: 30.0, color: Colors.blue),
-                  ),
-                ),
-              ),
-            ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text(' My Profile '),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.edit),
-              title: const Text(' Edit Profile '),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text('LogOut'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
     );
   }
 
