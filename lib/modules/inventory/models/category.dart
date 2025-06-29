3518c183f9ba920e422da56eb5a6c90558ccf708
Crescent Sambila <crescent.sambila@gmail.com>
2025-06-30 02:49:01 +0300

    Refactor Category model to use optional parameters for id and createdAt, and update toMap and fromMap methods for better data handling

--- lib/modules/inventory/models/category.dart
+++ lib/modules/inventory/models/category.dart
@@ -1,11 +1,37 @@
+import 'package:hive/hive.dart';
+import 'package:uuid/uuid.dart';
+
+part 'category.g.dart';
+
+@HiveType(typeId: 1)
 class Category {
+  @HiveField(0)
   final String id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final DateTime createdAt;
 
   Category({
-    required this.id,
+    String? id,
     required this.name,
-    required this.createdAt,
-  });
+    DateTime? createdAt,
+  })  : id = id ?? const Uuid().v4(),
+        createdAt = createdAt ?? DateTime.now();
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'createdAt': createdAt.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Category.fromMap(Map<String, dynamic> map) {
+    return Category(
+      id: map['id'],
+      name: map['name'],
+      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt']),
+    );
+  }
 }
