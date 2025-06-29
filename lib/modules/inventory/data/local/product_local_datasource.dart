680d8c48051141cf9f4bd11b9b23b99f14114125
Crescent Sambila <crescent.sambila@gmail.com>
2025-06-30 02:48:53 +0300

    Implement CRUD operations for Product and Category in ProductLocalDataSource

--- lib/modules/inventory/data/local/product_local_datasource.dart
+++ lib/modules/inventory/data/local/product_local_datasource.dart
@@ -1,3 +1,7 @@
+import 'package:hive/hive.dart';
+import '../../models/product.dart';
+import '../../models/category.dart';
+
 class ProductLocalDataSource {
   final Box<Product> productBox;
   final Box<Category> categoryBox;
@@ -11,9 +15,27 @@ class ProductLocalDataSource {
     return productBox.values.toList();
   }
 
+  Future<List<Category>> getCategories() async {
+    return categoryBox.values.toList();
+  }
+
   Future<void> addProduct(Product product) async {
     await productBox.put(product.id, product);
   }
 
-  // TODO: Add other CRUD operations
+  Future<void> updateProduct(Product product) async {
+    await productBox.put(product.id, product);
+  }
+
+  Future<void> deleteProduct(String productId) async {
+    await productBox.delete(productId);
+  }
+
+  Future<void> addCategory(Category category) async {
+    await categoryBox.put(category.id, category);
+  }
+
+  Future<void> deleteCategory(String categoryId) async {
+    await categoryBox.delete(categoryId);
+  }
 }
