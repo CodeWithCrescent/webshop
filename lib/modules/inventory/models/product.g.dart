5c31aa9da25afb62e40a1c73fde37c0644b25814
Crescent Geniius <crescentbeatz31@gmail.com>
2025-06-30 02:02:07 +0300

    Update pubspec.yaml to include build_runner and hive_generator; add generated TypeAdapters for Category and Product models

---
+++ lib/modules/inventory/models/product.g.dart
@@ -0,0 +1,65 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'product.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProductAdapter extends TypeAdapter<Product> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Product read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Product(
+      id: fields[0] as String?,
+      code: fields[1] as String,
+      name: fields[2] as String,
+      category: fields[3] as String,
+      price: fields[4] as double,
+      taxCategory: fields[5] as int,
+      stock: fields[6] as int,
+      createdAt: fields[7] as DateTime?,
+      updatedAt: fields[8] as DateTime?,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Product obj) {
+    writer
+      ..writeByte(9)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.code)
+      ..writeByte(2)
+      ..write(obj.name)
+      ..writeByte(3)
+      ..write(obj.category)
+      ..writeByte(4)
+      ..write(obj.price)
+      ..writeByte(5)
+      ..write(obj.taxCategory)
+      ..writeByte(6)
+      ..write(obj.stock)
+      ..writeByte(7)
+      ..write(obj.createdAt)
+      ..writeByte(8)
+      ..write(obj.updatedAt);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProductAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
