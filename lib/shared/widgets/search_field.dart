2dec817ca2215b9928bf945d1b14c92e79eb4b4d
Crescent Sambila <crescent.sambila@gmail.com>
2025-06-30 21:52:24 +0300

    Add optional controller parameter to SearchField for enhanced flexibility

--- lib/shared/widgets/search_field.dart
+++ lib/shared/widgets/search_field.dart
@@ -4,21 +4,25 @@ import '../../core/theme/app_theme.dart';
 class SearchField extends StatelessWidget {
   final String hintText;
   final ValueChanged<String> onChanged;
+  final TextEditingController? controller;
 
   const SearchField({
     super.key,
     required this.hintText,
     required this.onChanged,
+    this.controller,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.lightTheme;
-    
+
     return TextField(
+      controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
-        prefixIcon: Icon(Icons.search, color: theme.colorScheme.onSurface.withOpacity(0.5)),
+        prefixIcon: Icon(Icons.search,
+            color: theme.colorScheme.onSurface.withOpacity(0.5)),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(12),
           borderSide: BorderSide.none,
