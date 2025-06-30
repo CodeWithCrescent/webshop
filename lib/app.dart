c75ef3f283626efcfba27d51573a0717035afa69
Crescent Sambila <crescent.sambila@gmail.com>
2025-06-30 21:47:40 +0300

    Add routes for company profile, customers, receipts, and z-reports pages

--- lib/app.dart
+++ lib/app.dart
@@ -3,11 +3,15 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:webshop/modules/dashboard/dashboard_page.dart';
 import 'package:webshop/modules/inventory/pages/inventory_page.dart';
+import 'package:webshop/modules/settings/pages/company_profile_page.dart'; // Add this import
 import 'package:webshop/shared/layout_page.dart';
 import 'core/localization/app_localizations.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 import 'modules/auth/login_page.dart';
+import 'modules/customers/customers_page.dart';
+import 'modules/receipts/pages/receipts_page.dart';
+import 'modules/zreport/zreports_page.dart';
 import 'shared/widgets/glass_container.dart';
 
 class WebShopApp extends StatelessWidget {
@@ -29,6 +33,10 @@ class WebShopApp extends StatelessWidget {
             '/layout': (context) => const LayoutPage(),
             '/dashboard': (context) => const DashboardPage(),
             '/inventory': (context) => const InventoryPage(),
+            '/company-profile': (context) => const CompanyProfilePage(),
+            '/customers': (context) => const CustomersPage(),
+            '/receipts': (context) => const ReceiptsPage(),
+            '/z-reports': (context) => const ZReportsPage(),
           },
           localizationsDelegates: const [
             AppLocalizations.delegate,
