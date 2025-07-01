e126e19678777425507ccf0b3e96fbb046573d6b
Crescent Geniius <crescentbeatz31@gmail.com>
2025-07-01 07:27:27 +0300

    ---

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
+import 'package:webshop/modules/customers/pages/customers_page.dart';
 import 'package:webshop/modules/dashboard/dashboard_page.dart';
 import 'package:webshop/modules/inventory/pages/inventory_page.dart';
 import 'package:webshop/modules/settings/pages/company_profile_page.dart'; // Add this import
@@ -9,10 +10,8 @@ import 'core/localization/app_localizations.dart';
 import 'core/theme/app_theme.dart';
 import 'core/theme/theme_provider.dart';
 import 'modules/auth/login_page.dart';
-import 'modules/customers/customers_page.dart';
 import 'modules/receipts/pages/receipts_page.dart';
 import 'modules/zreport/zreports_page.dart';
-import 'shared/widgets/glass_container.dart';
 
 class WebShopApp extends StatelessWidget {
   const WebShopApp({super.key});
@@ -46,9 +45,7 @@ class WebShopApp extends StatelessWidget {
           ],
           supportedLocales: AppLocalizations.supportedLocales,
           builder: (context, child) {
-            return GlassContainer(
-              child: child!,
-            );
+            return child!;
           },
         );
       },
