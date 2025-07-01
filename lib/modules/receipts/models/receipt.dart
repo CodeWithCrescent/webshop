57fdbaa528006f1e4e606a6bded0cd45d389e75e
Crescent Sambila <crescent.sambila@gmail.com>
2025-07-01 15:32:19 +0300

    Refactor Receipt and ReceiptItem classes: update field names and improve data handling

--- lib/modules/receipts/models/receipt.dart
+++ lib/modules/receipts/models/receipt.dart
@@ -1,117 +1,123 @@
-import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
 
-part 'receipt.g.dart';
+class Receipt {
+  final String receiptNumber;
+  final String receiptDate;
+  final String receiptTime;
+  final String verificationCode;
+  final String zNumber;
+  final String verificationLink;
+  final String isDemo;
+  final String vrn;
+  final num totalExclOfTax;
+  final num totalTax;
+  final num totalInclOfTax;
+  final num discount;
+  final String customerIdType;
+  final String customerIdNumber;
+  final String customerName;
+  final String customerMobile;
+  final List<ReceiptItem> items;
 
-@HiveType(typeId: 4)
+  Receipt({
+    required this.receiptNumber,
+    required this.receiptDate,
+    required this.receiptTime,
+    required this.verificationCode,
+    required this.zNumber,
+    required this.verificationLink,
+    required this.isDemo,
+    required this.vrn,
+    required this.totalExclOfTax,
+    required this.totalTax,
+    required this.totalInclOfTax,
+    required this.discount,
+    required this.customerIdType,
+    required this.customerIdNumber,
+    required this.customerName,
+    required this.customerMobile,
+    required this.items,
+  });
+
+  String get formattedDate {
+    final date = DateTime.parse(receiptDate);
+    return DateFormat('d MMMM y').format(date);
+  }
+
+  factory Receipt.fromMap(Map<String, dynamic> map) {
+    return Receipt(
+      receiptNumber: map['receipt_number'] ?? '',
+      receiptDate: map['receipt_date'] ?? '',
+      receiptTime: map['receipt_time'] ?? '',
+      verificationCode: map['verificationcode'] ?? '',
+      zNumber: map['znum'] ?? '',
+      verificationLink: map['verif_link'] ?? '',
+      isDemo: map['isdemo'] ?? 'no',
+      vrn: map['vrn'] ?? '',
+      totalExclOfTax: num.tryParse(map['total_excl_of_tax']?.toString() ?? '0') ?? 0,
+      totalTax: num.tryParse(map['total_tax']?.toString() ?? '0') ?? 0,
+      totalInclOfTax: num.tryParse(map['total_incl_of_tax']?.toString() ?? '0') ?? 0,
+      discount: num.tryParse(map['discount']?.toString() ?? '0') ?? 0,
+      customerIdType: map['customer_id_type'] ?? '',
+      customerIdNumber: map['customer_id_number'] ?? '',
+      customerName: map['customer_name'] ?? '',
+      customerMobile: map['customer_mobile'] ?? '',
+      items: List<ReceiptItem>.from(
+        (map['items'] ?? []).map((x) => ReceiptItem.fromMap(x))),
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'receipt_number': receiptNumber,
+      'receipt_date': receiptDate,
+      'receipt_time': receiptTime,
+      'verificationcode': verificationCode,
+      'znum': zNumber,
+      'verif_link': verificationLink,
+      'isdemo': isDemo,
+      'vrn': vrn,
+      'total_excl_of_tax': totalExclOfTax,
+      'total_tax': totalTax,
+      'total_incl_of_tax': totalInclOfTax,
+      'discount': discount,
+      'customer_id_type': customerIdType,
+      'customer_id_number': customerIdNumber,
+      'customer_name': customerName,
+      'customer_mobile': customerMobile,
+      'items': items.map((item) => item.toMap()).toList(),
+    };
+  }
+}
+
 class ReceiptItem {
-  @HiveField(0)
-  final String itemcode;
-  @HiveField(1)
-  final String itemdesc;
-  @HiveField(2)
-  final int itemqty;
-  @HiveField(3)
-  final double amount;
+  final String itemCode;
+  final String itemDescription;
+  final num itemQuantity;
+  final num amount;
 
   ReceiptItem({
-    required this.itemcode,
-    required this.itemdesc,
-    required this.itemqty,
+    required this.itemCode,
+    required this.itemDescription,
+    required this.itemQuantity,
     required this.amount,
   });
 
   factory ReceiptItem.fromMap(Map<String, dynamic> map) {
     return ReceiptItem(
-      itemcode: map['itemcode'],
-      itemdesc: map['itemdesc'],
-      itemqty: int.parse(map['itemqty'].toString()),
-      amount: double.parse(map['amount'].toString()),
+      itemCode: map['itemcode'] ?? '',
+      itemDescription: map['itemdesc'] ?? '',
+      itemQuantity: num.tryParse(map['itemqty']?.toString() ?? '0') ?? 0,
+      amount: num.tryParse(map['amount']?.toString() ?? '0') ?? 0,
     );
   }
-}
 
-@HiveType(typeId: 5)
-class Receipt {
-  @HiveField(0)
-  final String receipt_number;
-  @HiveField(1)
-  final String verificationcode;
-  @HiveField(2)
-  final String receipt_time;
-  @HiveField(3)
-  final DateTime receipt_date;
-  @HiveField(4)
-  final String znum;
-  @HiveField(5)
-  final String verif_link;
-  @HiveField(6)
-  final String isdemo;
-  @HiveField(7)
-  final String vrn;
-  @HiveField(8)
-  final double total_excl_of_tax;
-  @HiveField(9)
-  final double total_tax;
-  @HiveField(10)
-  final double total_incl_of_tax;
-  @HiveField(11)
-  final double discount;
-  @HiveField(12)
-  final String customer_id_type;
-  @HiveField(13)
-  final String customer_id_number;
-  @HiveField(14)
-  final String customer_name;
-  @HiveField(15)
-  final String customer_mobile;
-  @HiveField(16)
-  final List<ReceiptItem> items;
-
-  Receipt({
-    required this.receipt_number,
-    required this.verificationcode,
-    required this.receipt_time,
-    required this.receipt_date,
-    required this.znum,
-    required this.verif_link,
-    required this.isdemo,
-    required this.vrn,
-    required this.total_excl_of_tax,
-    required this.total_tax,
-    required this.total_incl_of_tax,
-    required this.discount,
-    required this.customer_id_type,
-    required this.customer_id_number,
-    required this.customer_name,
-    required this.customer_mobile,
-    required this.items,
-  });
-
-  String get formattedDate => DateFormat('dd MMM yyyy').format(receipt_date);
-
-  String get formattedTime => receipt_time;
-
-  factory Receipt.fromMap(Map<String, dynamic> map) {
-    return Receipt(
-      receipt_number: map['receipt_number'],
-      verificationcode: map['verificationcode'],
-      receipt_time: map['receipt_time'],
-      receipt_date: DateTime.parse(map['receipt_date']),
-      znum: map['znum'],
-      verif_link: map['verif_link'],
-      isdemo: map['isdemo'],
-      vrn: map['vrn'],
-      total_excl_of_tax: double.parse(map['total_excl_of_tax'].toString()),
-      total_tax: double.parse(map['total_tax'].toString()),
-      total_incl_of_tax: double.parse(map['total_incl_of_tax'].toString()),
-      discount: double.parse(map['discount'].toString()),
-      customer_id_type: map['customer_id_type'],
-      customer_id_number: map['customer_id_number'],
-      customer_name: map['customer_name'],
-      customer_mobile: map['customer_mobile'],
-      items: List<ReceiptItem>.from(
-          map['items'].map((x) => ReceiptItem.fromMap(x))),
-    );
+  Map<String, dynamic> toMap() {
+    return {
+      'itemcode': itemCode,
+      'itemdesc': itemDescription,
+      'itemqty': itemQuantity,
+      'amount': amount,
+    };
   }
 }
