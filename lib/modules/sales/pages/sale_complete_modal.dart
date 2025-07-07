36a2fbed00c5686dbbc0ffd2b45ce6582b9cd258
Crescent Sambila <crescent.sambila@gmail.com>
2025-07-07 06:13:52 +0300

    fixes

--- lib/modules/sales/pages/sale_complete_modal.dart
+++ lib/modules/sales/pages/sale_complete_modal.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:webshop/modules/sales/providers/sales_provider.dart';
 
