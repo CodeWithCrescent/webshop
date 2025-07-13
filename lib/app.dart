8afd2b3aedf9f05fe64391018d431c00389223b8
Crescent Sambila <crescent.sambila@gmail.com>
2025-07-13 06:24:58 +0300

    fix

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // <-- Needed for SystemChrome
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
 import 'package:webshop/modules/auth/splash_page.dart';
@@ -6,7 +7,7 @@ import 'package:webshop/modules/customers/pages/customers_page.dart';
 import 'package:webshop/modules/dashboard/dashboard_page.dart';
 import 'package:webshop/modules/inventory/pages/inventory_page.dart';
 import 'package:webshop/modules/sales/pages/sales_page.dart';
-import 'package:webshop/modules/settings/pages/business_profile_page.dart'; // Add this import
+import 'package:webshop/modules/settings/pages/business_profile_page.dart';
 import 'package:webshop/shared/layout_page.dart';
 import 'core/localization/app_localizations.dart';
 import 'core/theme/app_theme.dart';
@@ -19,10 +20,25 @@ class WebShopApp extends StatelessWidget {
   WebShopApp({super.key});
 
   final navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     return Consumer<ThemeProvider>(
       builder: (context, themeProvider, child) {
+        final theme = themeProvider.themeMode == ThemeMode.dark
+            ? AppTheme.darkTheme
+            : AppTheme.lightTheme;
+
+        // Set status bar color to match AppBar color
+        SystemChrome.setSystemUIOverlayStyle(
+          SystemUiOverlayStyle(
+            statusBarColor: theme.appBarTheme.backgroundColor ?? Colors.transparent,
+            statusBarIconBrightness: theme.brightness == Brightness.dark
+                ? Brightness.light
+                : Brightness.dark,
+          ),
+        );
+
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           navigatorKey: navigatorKey,
